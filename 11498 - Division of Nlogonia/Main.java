a61812550223fb9aafb78c999b37b03aa2e8ec04
Jake Arata <jakeearata@gmail.com>
2019-03-01 08:54:47 -0600

    Update Main.java

--- 11498 - Division of Nlogonia/Main.java
+++ 11498 - Division of Nlogonia/Main.java
@@ -1,11 +1,20 @@
 import java.util.*;
+import java.lang.*;
 public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int runs = sc.nextInt();
+        int xDiv = sc.nextInt();
+        int yDiv = sc.nextInt();
         for(int i = 0; i < runs; i++){
-            int xDiv = sc.nextInt();
-            int yDiv = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if(x = xDiv || y = yDiv){
+                out.println("divisa");
+                continue;
+            }
+            if
+            
             
         }
 
