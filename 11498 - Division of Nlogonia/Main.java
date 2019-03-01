1241eacfc4d953a496d74d95123d58815b67f411
Jake Arata <jakeearata@gmail.com>
2019-03-01 08:50:22 -0600

    Create Main.java

---
+++ 11498 - Division of Nlogonia/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int runs = sc.nextInt();
+        for(int i = 0; i < runs; i++){
+            int xDiv = sc.nextInt();
+            int yDiv = sc.nextInt();
+            
+        }
+
+    }
+}
